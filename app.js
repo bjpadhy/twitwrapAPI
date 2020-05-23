2185871befe2fc48c7308f0b78c23666d8f39ea8
bjpadhy <padhybiswaranjan@live.com>
2020-05-24 02:06:05 +0530

    Updated port use

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 //Start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Listening on port 3000");
 });
 
